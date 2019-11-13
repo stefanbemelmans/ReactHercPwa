618a30c1909b0957c91ce211afad794b0cc2b9b9
stefanbemelmans <stefanbemelmans898@gmail.com>
2019-11-12 20:14:55 -0500

    got the store working

--- src/Features/EdgeContext/reducers/EdgeContextReducers.js
+++ src/Features/EdgeContext/reducers/EdgeContextReducers.js
@@ -1,34 +1,45 @@
 // Basic App State
 // import * as EdgeContextActionCreators from "../actions/EdgeContextActions"
-var EdgeContextState = {
-    loggedIn: false,
-    edgeAccount: null,
-    edgeContext: null,
+var InitialState = {
+  loggedIn: false,
+  edgeAccount: null,
+  edgeContext: null,
 
 }
 
-const EdgeContextReducers = (EdgeContextState, action) => {
-  switch (action.type) {
-    case 'Set_Edge_Context':
-      return{
-        ...EdgeContextState, 
+// Really the only function that deals with context, so I decided to separate it. 
+function SetEdgeContextReducer(EdgeContextState = InitialState, action) {
+
+  return action.type === "Set_Edge_Context" ?
+    Object.assign({}, EdgeContextState,
+      {
+        ...EdgeContextState,
         edgeContext: action.edgeContext
-      }
+      })
+    :
+    EdgeContextState
+}
+
+
+function EdgeLoginReducer(EdgeContextState = InitialState, action) {
+  switch (action.type) {
     case 'Edge_Login':
-      return{ 
-        ...EdgeContextState, 
-        loggedIn: true,
-        edgeAccount : action.edgeAccount
-      }
+      return Object.assign({}, EdgeContextState,
+        {
+          ...EdgeContextState,
+          loggedIn: true,
+          edgeAccount: action.edgeAccount
+        })
     case 'Edge_Logout':
-      return{ 
-        ...EdgeContextState, 
-        loggedIn: false,
-        edgeAccount: null
-      }
+      return Object.assign({}, EdgeContextState,
+        {
+          ...EdgeContextState,
+          loggedIn: false,
+          edgeAccount: null
+        })
     default:
       return EdgeContextState
   }
 }
 
-export default EdgeContextReducers;
+export default [SetEdgeContextReducer, EdgeLoginReducer]
